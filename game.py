550745fe7297f5566c30d8fe01be4c02c9f5df94
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-01 06:40:19 +0200

    Improve Game:  multiplication table As a function

--- game.py
+++ game.py
@@ -8,3 +8,16 @@ for x in range(start,end+1):
     for y in range(1,11):
         print(f"{x} * {y} = {x*y}")
     print("----------------------") 
+
+
+#Improve Game:  multiplication table As a function
+#  start , end ---> multiplication table [start:end] 
+
+def mul_table(start,end,mstart=1,mstop=10):
+    for x in range(start,end+1):
+        for y in range(mstart,mstop+1):
+            print(f"{x} * {y} = {x*y}")
+        print("----------------------")
+
+        
+mul_table(2,6,2,20)
